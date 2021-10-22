82d9eb098680ca79d4e54e5e9eb6e39c25b8c74b
王志杰 <1306082199@qq.com>
2021-10-22 17:56:10 +0800

    美团 Vue oauth权限验证

--- meituan-services/meituan-thirdparty/src/main/java/com/ddg/meituan/thridparty/Service/impl/OSSServiceImpl.java
+++ meituan-services/meituan-thirdparty/src/main/java/com/ddg/meituan/thridparty/Service/impl/OSSServiceImpl.java
@@ -36,7 +36,9 @@ public class OSSServiceImpl implements OSSService {
     @Override
     public String uploadFileAvatar(MultipartFile file) {
 
-        if (file == null) return null;
+        if (file == null) {
+            return null;
+        }
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = ossProperties.getEndpoint();
         // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
--- meituan-services/meituan-thirdparty/src/main/java/com/ddg/meituan/thridparty/controller/MinioController.java
+++ meituan-services/meituan-thirdparty/src/main/java/com/ddg/meituan/thridparty/controller/MinioController.java
@@ -1,46 +1,47 @@
 package com.ddg.meituan.thridparty.controller;
 
-
-
 import com.ddg.meituan.common.utils.R;
-import com.ddg.meituan.thridparty.Service.OSSService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.ddg.miniostarter.MinioHelper;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
 
 /**
  * Description:
  * ========================================================================
  * ------------------------------------------------------------------------
  *
- * @author Edison
+ * @author wzj
  * @version 1.0
  * <p>
  * ========================================================================
- * @date 2021/1/30 17:58
+ * @date 2021/10/22 10:46
  * @email: wangzhijie0908@gmail.com
  */
 @RestController
-@RequestMapping("/thirdparty/oss")
-public class OSSController {
+@RequestMapping("/thirdparty/minio")
+public class MinioController {
 
-    @Autowired
-    private OSSService ossService;
-
+    @Resource
+    private MinioHelper minioHelper;
 
     @PostMapping("/upload")
-    public R uploadFile(MultipartFile file){
+    public R uploadFile(@RequestParam("file") MultipartFile file){
         // 进行调用 service中的方法  返回的是文件的url地址
 
-        String url =  ossService.uploadFileAvatar(file);
+        String url = null;
+        try {
+            url = minioHelper.uploadFile(file.getInputStream(), file.getOriginalFilename(),
+                    file.getContentType(), file.getSize());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return R.ok().put("data", url);
     }
-
-
-
 
 
 }
