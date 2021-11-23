d6afda1b9440f404dbbd102f649998c1ce6c6db5
王志杰 <1306082199@qq.com>
2021-11-23 10:04:59 +0800

    美团 权限实现

--- meituan-common/meituan-base/src/main/java/com/ddg/meituan/common/utils/Constant.java
+++ meituan-common/meituan-base/src/main/java/com/ddg/meituan/common/utils/Constant.java
@@ -12,5 +12,33 @@ public class Constant {
      */
     public static final String ASC = "asc";
 
+    /**
+     * 云服务商
+     */
+    public enum CloudService {
+        /**
+         * 七牛云
+         */
+        QINIU(1),
+        /**
+         * 阿里云
+         */
+        ALIYUN(2),
+        /**
+         * 腾讯云
+         */
+        QCLOUD(3);
+
+        private int value;
+
+        CloudService(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
 
 }
