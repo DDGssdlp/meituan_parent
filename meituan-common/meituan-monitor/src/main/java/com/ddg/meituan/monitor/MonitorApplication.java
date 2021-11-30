9b64d3aa4984f256cc223fe8e935a17a8d96f768
王志杰 <1306082199@qq.com>
2021-11-30 09:57:24 +0800

    美团 监控实现

---
+++ meituan-common/meituan-monitor/src/main/java/com/ddg/meituan/monitor/MonitorApplication.java
@@ -0,0 +1,29 @@
+package com.ddg.meituan.monitor;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Description:
+ * ========================================================================
+ * ------------------------------------------------------------------------
+ *
+ * @author wzj
+ * @version 1.0
+ * <p>
+ * ========================================================================
+ * @date 2021/11/29 18:18
+ * @email: wangzhijie0908@gmail.com
+ */
+@EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
+public class MonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+    }
+
+}
